95b6bfbee780eecd7a9fb812bbf1d8e1825d7122
abhijith <xperio135@gmail.com>
2023-10-14 22:49:23 +0530

    Formatted and Center Aligned the Hello World text

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,10 +1,25 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Text(
-        'Hello World'
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 360,
+            color: Colors.cyan,
+            child: Text(
+            'Hello World',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.black,
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
